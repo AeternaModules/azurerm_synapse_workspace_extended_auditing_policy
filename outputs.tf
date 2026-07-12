f5edafcf1500e81859be8b33107b75545bc27a67
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:21:10 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,31 +1,26 @@
-output "synapse_workspace_extended_auditing_policies" {
-  description = "All synapse_workspace_extended_auditing_policy resources"
-  value       = azurerm_synapse_workspace_extended_auditing_policy.synapse_workspace_extended_auditing_policies
-  sensitive   = true
-}
 output "synapse_workspace_extended_auditing_policies_log_monitoring_enabled" {
-  description = "List of log_monitoring_enabled values across all synapse_workspace_extended_auditing_policies"
-  value       = [for k, v in azurerm_synapse_workspace_extended_auditing_policy.synapse_workspace_extended_auditing_policies : v.log_monitoring_enabled]
+  description = "Map of log_monitoring_enabled values across all synapse_workspace_extended_auditing_policies, keyed the same as var.synapse_workspace_extended_auditing_policies"
+  value       = { for k, v in azurerm_synapse_workspace_extended_auditing_policy.synapse_workspace_extended_auditing_policies : k => v.log_monitoring_enabled }
 }
 output "synapse_workspace_extended_auditing_policies_retention_in_days" {
-  description = "List of retention_in_days values across all synapse_workspace_extended_auditing_policies"
-  value       = [for k, v in azurerm_synapse_workspace_extended_auditing_policy.synapse_workspace_extended_auditing_policies : v.retention_in_days]
+  description = "Map of retention_in_days values across all synapse_workspace_extended_auditing_policies, keyed the same as var.synapse_workspace_extended_auditing_policies"
+  value       = { for k, v in azurerm_synapse_workspace_extended_auditing_policy.synapse_workspace_extended_auditing_policies : k => v.retention_in_days }
 }
 output "synapse_workspace_extended_auditing_policies_storage_account_access_key" {
-  description = "List of storage_account_access_key values across all synapse_workspace_extended_auditing_policies"
-  value       = [for k, v in azurerm_synapse_workspace_extended_auditing_policy.synapse_workspace_extended_auditing_policies : v.storage_account_access_key]
+  description = "Map of storage_account_access_key values across all synapse_workspace_extended_auditing_policies, keyed the same as var.synapse_workspace_extended_auditing_policies"
+  value       = { for k, v in azurerm_synapse_workspace_extended_auditing_policy.synapse_workspace_extended_auditing_policies : k => v.storage_account_access_key }
   sensitive   = true
 }
 output "synapse_workspace_extended_auditing_policies_storage_account_access_key_is_secondary" {
-  description = "List of storage_account_access_key_is_secondary values across all synapse_workspace_extended_auditing_policies"
-  value       = [for k, v in azurerm_synapse_workspace_extended_auditing_policy.synapse_workspace_extended_auditing_policies : v.storage_account_access_key_is_secondary]
+  description = "Map of storage_account_access_key_is_secondary values across all synapse_workspace_extended_auditing_policies, keyed the same as var.synapse_workspace_extended_auditing_policies"
+  value       = { for k, v in azurerm_synapse_workspace_extended_auditing_policy.synapse_workspace_extended_auditing_policies : k => v.storage_account_access_key_is_secondary }
 }
 output "synapse_workspace_extended_auditing_policies_storage_endpoint" {
-  description = "List of storage_endpoint values across all synapse_workspace_extended_auditing_policies"
-  value       = [for k, v in azurerm_synapse_workspace_extended_auditing_policy.synapse_workspace_extended_auditing_policies : v.storage_endpoint]
+  description = "Map of storage_endpoint values across all synapse_workspace_extended_auditing_policies, keyed the same as var.synapse_workspace_extended_auditing_policies"
+  value       = { for k, v in azurerm_synapse_workspace_extended_auditing_policy.synapse_workspace_extended_auditing_policies : k => v.storage_endpoint }
 }
 output "synapse_workspace_extended_auditing_policies_synapse_workspace_id" {
-  description = "List of synapse_workspace_id values across all synapse_workspace_extended_auditing_policies"
-  value       = [for k, v in azurerm_synapse_workspace_extended_auditing_policy.synapse_workspace_extended_auditing_policies : v.synapse_workspace_id]
+  description = "Map of synapse_workspace_id values across all synapse_workspace_extended_auditing_policies, keyed the same as var.synapse_workspace_extended_auditing_policies"
+  value       = { for k, v in azurerm_synapse_workspace_extended_auditing_policy.synapse_workspace_extended_auditing_policies : k => v.synapse_workspace_id }
 }
 
